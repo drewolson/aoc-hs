ecbba69eb73d1188bbe10b7aa8394bae63c624a3
drew <drew@drewolson.org>
2021-11-28 16:23:00 -0600

    More cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,13 @@
 module Main where
 
+import Aoc.Prelude
 import Aoc.Solution.Day01 qualified as Day01
 import Control.Applicative ((<**>))
-import Data.Text (Text, justifyRight, pack, unpack)
+import Data.Text (Text)
+import Data.Text qualified as T
 import Data.Text.IO qualified as Text.IO
-import Options.Applicative (Parser, ParserInfo, auto, execParser, fullDesc, header, help, helper, info, long, option, progDesc, short)
+import Options.Applicative (Parser, ParserInfo)
+import Options.Applicative qualified as A
 
 data Args = Args
   { day :: Int,
@@ -14,32 +17,32 @@ data Args = Args
 parseArgs :: Parser Args
 parseArgs =
   Args
-    <$> option
-      auto
-      ( long "day"
-          <> short 'd'
-          <> help "day to run (1 - 25)"
+    <$> A.option
+      A.auto
+      ( A.long "day"
+          <> A.short 'd'
+          <> A.help "day to run (1 - 25)"
       )
-    <*> option
-      auto
-      ( long "part"
-          <> short 'p'
-          <> help "part to run (1 or 2)"
+    <*> A.option
+      A.auto
+      ( A.long "part"
+          <> A.short 'p'
+          <> A.help "part to run (1 or 2)"
       )
 
 parseInfoArgs :: ParserInfo Args
 parseInfoArgs =
-  info
-    (parseArgs <**> helper)
-    ( fullDesc
-        <> progDesc "run aoc solution"
-        <> header "aoc-exe - run aoc solution"
+  A.info
+    (parseArgs <**> A.helper)
+    ( A.fullDesc
+        <> A.progDesc "run aoc solution"
+        <> A.header "aoc-exe - run aoc solution"
     )
 
 readInput :: Args -> IO Text
 readInput Args {day} =
-  let dayText = justifyRight 2 '0' $ pack (show day)
-   in Text.IO.readFile $ "./data/day" <> unpack dayText <> ".txt"
+  let dayText = T.justifyRight 2 '0' $ tshow day
+   in Text.IO.readFile $ "./data/day" <> T.unpack dayText <> ".txt"
 
 runSolution :: Text -> Args -> IO Text
 runSolution input = \case
@@ -48,7 +51,7 @@ runSolution input = \case
 
 main :: IO ()
 main = do
-  args <- execParser parseInfoArgs
+  args <- A.execParser parseInfoArgs
   input <- readInput args
   result <- runSolution input args
 
