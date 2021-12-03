700822b880bd8eb81dd29d05d5eb3d69238a654d
drew <drew@drewolson.org>
2021-12-03 08:38:54 -0600

    Day03 clean up

--- src/Aoc/Solution/Day03.hs
+++ src/Aoc/Solution/Day03.hs
@@ -15,6 +15,9 @@ binToInt = foldl' (\acc x -> acc * 2 + digitToInt x) 0
 mostCommon :: String -> Char
 mostCommon = head . maximumBy (comparing length) . group . sort
 
+leastCommon :: String -> Char
+leastCommon = flipBit . mostCommon
+
 flipBit :: Char -> Char
 flipBit '1' = '0'
 flipBit _ = '1'
@@ -23,21 +26,20 @@ part1 :: String -> Int
 part1 input =
   let d = transpose $ lines input
       g = binToInt $ fmap mostCommon d
-      e = binToInt $ fmap (flipBit . mostCommon) d
+      e = binToInt $ fmap leastCommon d
    in g * e
 
-findRating :: ([Char] -> Char) -> [String] -> Int -> Int
-findRating _ [s] _ = binToInt s
-findRating p l n =
-  let t = transpose l
-      ns = t !! n
-      d = p ns
-      l' = filter ((== d) . (!! n)) l
-   in findRating p l' (n + 1)
+findRating :: ([Char] -> Char) -> Int -> [String] -> Int
+findRating _ _ [s] = binToInt s
+findRating p n l =
+  let d = transpose l !! n
+      c = p d
+      l' = filter ((== c) . (!! n)) l
+   in findRating p (n + 1) l'
 
 part2 :: String -> Int
 part2 input =
   let l = lines input
-      o2 = findRating mostCommon l 0
-      co2 = findRating (flipBit . mostCommon) l 0
+      o2 = findRating mostCommon 0 l
+      co2 = findRating leastCommon 0 l
    in o2 * co2
