0839c1a84b23f0e30472b35c14490c7adf60bb08
drew <drew@drewolson.org>
2021-12-10 08:58:50 -0600

    Clean up

--- src/Aoc/Solution/Day10.hs
+++ src/Aoc/Solution/Day10.hs
@@ -4,6 +4,7 @@ module Aoc.Solution.Day10
   )
 where
 
+import Data.Either (lefts, rights)
 import Data.List (sort)
 
 isOpen :: Char -> Bool
@@ -16,49 +17,39 @@ toClose = \case
   '{' -> '}'
   _ -> '>'
 
-syntaxScore :: String -> Int
+charScore :: Char -> Int
+charScore = \case
+  ')' -> 1
+  ']' -> 2
+  '}' -> 3
+  _ -> 4
+
+completionScore :: String -> Int
+completionScore = foldl (\acc c -> acc * 5 + charScore c) 0
+
+errorScore :: Char -> Int
+errorScore = \case
+  ')' -> 3
+  ']' -> 57
+  '}' -> 1197
+  _ -> 25137
+
+syntaxScore :: String -> Either Int Int
 syntaxScore = go []
   where
-    go :: String -> String -> Int
-    go _ [] = 0
-    go [] (a : b)
-      | isOpen a = go [toClose a] b
-      | otherwise = 0
+    go :: String -> String -> Either Int Int
+    go acc [] = Left $ completionScore acc
+    go [] (a : b) = go [toClose a] b
     go a@(ha : ta) (hb : tb)
       | isOpen hb = go (toClose hb : a) tb
       | ha == hb = go ta tb
-      | otherwise =
-        case hb of
-          ')' -> 3
-          ']' -> 57
-          '}' -> 1197
-          _ -> 25137
-
-part1 :: String -> Int
-part1 = sum . fmap syntaxScore . lines
-
-correctScore :: String -> Int
-correctScore = go []
-  where
-    charScore :: Char -> Int
-    charScore = \case
-      ')' -> 1
-      ']' -> 2
-      '}' -> 3
-      _ -> 4
-
-    score :: String -> Int
-    score = foldl (\acc c -> acc * 5 + charScore c) 0
-
-    go :: String -> String -> Int
-    go acc [] = score acc
-    go [] (a : b) = go [toClose a] b
-    go a@(_ : ta) (hb : tb)
-      | isOpen hb = go (toClose hb : a) tb
-      | otherwise = go ta tb
+      | otherwise = Right $ errorScore hb
 
 middle :: [a] -> a
 middle as = as !! (length as `div` 2)
 
+part1 :: String -> Int
+part1 = sum . rights . fmap syntaxScore . lines
+
 part2 :: String -> Int
-part2 = middle . sort . fmap correctScore . filter ((== 0) . syntaxScore) . lines
+part2 = middle . sort . lefts . fmap syntaxScore . lines
