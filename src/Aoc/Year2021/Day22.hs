c410ae82584ed0471193ccc2c9091f154e5b6b6f
drew <drew@drewolson.org>
2021-12-22 15:48:55 -0600

    Model using boxes

--- src/Aoc/Year2021/Day22.hs
+++ src/Aoc/Year2021/Day22.hs
@@ -13,15 +13,13 @@ import Text.Megaparsec.Char (char, newline, space, string)
 data Action = On | Off
   deriving (Eq, Show)
 
-type Range = (Int, Int)
-
 type Point = (Int, Int, Int)
+
+type Box = (Point, Point)
 
 data Step = Step
   { action :: Action,
-    x :: Range,
-    y :: Range,
-    z :: Range
+    box :: Box
   }
   deriving (Eq, Show)
 
@@ -31,13 +29,17 @@ parseAction = On <$ string "on" <|> Off <$ string "off"
 parseRange :: Parser (Int, Int)
 parseRange = (,) <$> parseSignedInt <*> (string ".." *> parseSignedInt)
 
+parseBox :: Parser Box
+parseBox = do
+  (xmin, xmax) <- string "x=" *> parseRange <* char ','
+  (ymin, ymax) <- string "y=" *> parseRange <* char ','
+  (zmin, zmax) <- string "z=" *> parseRange
+
+  pure ((xmin, ymin, zmin), (xmax, ymax, zmax))
+
 parseStep :: Parser Step
 parseStep =
-  Step
-    <$> (parseAction <* space)
-    <*> (string "x=" *> parseRange <* char ',')
-    <*> (string "y=" *> parseRange <* char ',')
-    <*> (string "z=" *> parseRange)
+  Step <$> (parseAction <* space) <*> parseBox
 
 parseSteps :: Parser [Step]
 parseSteps = sepEndBy1 parseStep newline
@@ -49,8 +51,8 @@ isOn :: [Step] -> Point -> Bool
 isOn steps point = foldl' (transform point) False steps
   where
     inRange :: Point -> Step -> Bool
-    inRange (a, b, c) Step {x = (x1, x2), y = (y1, y2), z = (z1, z2)} =
-      x1 <= a && a <= x2 && y1 <= b && b <= y2 && z1 <= c && c <= z2
+    inRange (a, b, c) Step {box = ((xmin, ymin, zmin), (xmax, ymax, zmax))} =
+      xmin <= a && a <= xmax && ymin <= b && b <= ymax && zmin <= c && c <= zmax
 
     transform :: Point -> Bool -> Step -> Bool
     transform _ False Step {action = Off} = False
@@ -67,10 +69,11 @@ part1 input =
 part2 :: String -> Int
 part2 input =
   let steps = parseInput input
-      xmin = minimum $ fst . x <$> filter ((== On) . action) steps
-      xmax = maximum $ snd . x <$> filter ((== On) . action) steps
-      ymin = minimum $ fst . y <$> filter ((== On) . action) steps
-      ymax = maximum $ snd . y <$> filter ((== On) . action) steps
-      zmin = minimum $ fst . z <$> filter ((== On) . action) steps
-      zmax = maximum $ snd . z <$> filter ((== On) . action) steps
+      onBoxes = box <$> filter ((== On) . action) steps
+      xmin = minimum $ fmap (\((x, _, _), _) -> x) onBoxes
+      xmax = maximum $ fmap (\((_, _, _), (x, _, _)) -> x) onBoxes
+      ymin = minimum $ fmap (\((_, y, _), _) -> y) onBoxes
+      ymax = maximum $ fmap (\((_, _, _), (_, y, _)) -> y) onBoxes
+      zmin = minimum $ fmap (\((_, _, z), _) -> z) onBoxes
+      zmax = maximum $ fmap (\((_, _, _), (_, _, z)) -> z) onBoxes
    in length $ filter (isOn steps) $ [(x, y, z) | x <- [xmin .. xmax], y <- [ymin .. ymax], z <- [zmin .. zmax]]
