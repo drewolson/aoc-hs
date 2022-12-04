d1a630e8e0ebc72bdda2486ab49e9bd91169889c
drew <drew@drewolson.org>
2022-12-04 08:13:03 -0600

    Use maybe

--- aoc-year2022/src/Aoc/Year2022/Day04.hs
+++ aoc-year2022/src/Aoc/Year2022/Day04.hs
@@ -1,5 +1,3 @@
-{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
-
 module Aoc.Year2022.Day04
   ( part1,
     part2,
@@ -7,6 +5,7 @@ module Aoc.Year2022.Day04
 where
 
 import Data.List.Split (splitOn)
+import Data.Maybe (mapMaybe)
 import Data.Set (Set)
 import Data.Set qualified as Set
 
@@ -16,18 +15,20 @@ overlapped (a, b) = Set.isSubsetOf a b || Set.isSubsetOf b a
 overlapped' :: (Set Int, Set Int) -> Bool
 overlapped' (a, b) = not $ Set.null $ Set.intersection a b
 
-makeSet :: String -> Set Int
+makeSet :: String -> Maybe (Set Int)
 makeSet range =
-  let [a, b] = splitOn "-" range
-   in Set.fromList [read a .. read b]
+  case splitOn "-" range of
+    [a, b] -> Just $ Set.fromList [read a .. read b]
+    _ -> Nothing
 
-parse :: String -> (Set Int, Set Int)
+parse :: String -> Maybe (Set Int, Set Int)
 parse line =
-  let [a, b] = splitOn "," line
-   in (makeSet a, makeSet b)
+  case splitOn "," line of
+    [a, b] -> (,) <$> makeSet a <*> makeSet b
+    _ -> Nothing
 
 part1 :: String -> Int
-part1 = length . filter overlapped . fmap parse . lines
+part1 = length . filter overlapped . mapMaybe parse . lines
 
 part2 :: String -> Int
-part2 = length . filter overlapped' . fmap parse . lines
+part2 = length . filter overlapped' . mapMaybe parse . lines
