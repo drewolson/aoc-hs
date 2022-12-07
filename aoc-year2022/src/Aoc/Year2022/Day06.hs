aefeaf0c37beb2fdbb7ec53d87fa9532aadc4fd4
drew <drew@drewolson.org>
2022-12-06 21:33:38 -0600

    Reorder clauses

--- aoc-year2022/src/Aoc/Year2022/Day06.hs
+++ aoc-year2022/src/Aoc/Year2022/Day06.hs
@@ -7,12 +7,12 @@ where
 import Data.List (nub)
 
 findChunk :: Int -> Int -> String -> Int
+findChunk _ _ [] = 0
 findChunk n i l@(_ : t) =
   let c = take n l
    in if length c == n && nub c == c
         then i + n
         else findChunk n (i + 1) t
-findChunk _ _ [] = 0
 
 part1 :: String -> Int
 part1 = findChunk 4 0
